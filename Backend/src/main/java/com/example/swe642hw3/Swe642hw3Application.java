1e048586f23072953d45eb5aee6fad0ed6e3f216
MEDHA-BHARADWAJ <medhavkrishna@gmail.com>
2022-09-25 20:04:14 -0400

    updating

---
+++ Backend/src/main/java/com/example/swe642hw3/Swe642hw3Application.java
@@ -0,0 +1,13 @@
+package com.example.swe642hw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe642hw3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swe642hw3Application.class, args);
+    }
+
+}
